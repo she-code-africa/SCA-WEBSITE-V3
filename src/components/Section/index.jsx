17940497fb366691d074923c636915ba74f27191
EseAlli <essealli.ea@gmail.com>
2022-01-31 09:09:08 +0100

    created section component and added the component to home page

---
+++ src/components/Section/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Section = ({title, content, imgClass, className}) => {
+  return <section className="__shecode_block">
+      <div className="grid md:grid-cols-5 text-left xs:grid-row-4 md:grid-row-1">
+        <div className={`${imgClass} col-span-2`}>
+
+        </div>
+        <div className={`${className} col-span-3`}>
+          <h4 className="__shecode_title">{title}</h4>
+          <p className="__shecode_subtitle">{content}</p>
+        </div>
+      </div>
+  </section>;
+};
+
+export default Section;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,42 @@
 import React from "react";
-
 import Header from "../../components/Header";
+import Section from "../../components/Section";
 const Home = () => {
   return (
     <>
     <Header />
     <main>
-      homepage
+      <div className="container">
+        homepage
+        <Section 
+          title="Growing Slack Community"
+          content="Join over 10,000 Women in Tech across several African countries on our Slack community to network, stay updated, get help and grow in your career."
+          imgClass="home_side_identify justify-self-center"
+          className="order-last"
+        />
+
+        <Section 
+          title="Attend an Event"
+          content="Participate in any of our online or offline events and initiatives inluding bootcamps, Fireside chats, webinars, Mentorship programs and several more!"
+          imgClass="home_side_event justify-self-center"
+          className="-order-1 justify-self-center"
+        />
+
+        <Section 
+          title="Be a Partner or Sponsor"
+          content="Be a sponsor for our next event via a donation or Partner with our organisation to reach more women in tech across Africa"
+          imgClass="home_side_partner justify-self-center"
+          className="order-last"
+        />
+
+        <Section 
+          title="Follow Us on Social Media"
+          content="Stay updated with us and be the first to know about all our activities and events, by following our social media pages"
+          imgClass="home_side_follow justify-self-center"
+          className="-order-1 justify-self-end"
+        />
+
+      </div>
     </main>
     </>
   )
