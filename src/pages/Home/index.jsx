485247d316cbc46aa6a785f3b711a2fb767907da
EseAlli <essealli.ea@gmail.com>
2022-02-01 08:49:43 +0100

    Added button children to section component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,35 +6,44 @@ const Home = () => {
     <>
     <Header page={'home'} />
     <main>
-      <div className="container mx-auto px-12">
+      <div className="container mx-auto px-16">
         homepage
         <Section 
           title="Growing Slack Community"
           content="Join over 10,000 Women in Tech across several African countries on our Slack community to network, stay updated, get help and grow in your career."
-          imgClass="home_side_identify justify-self-center"
+          imgClass="home_side_identify justify-center"
           className="order-last"
-        />
+        >
+          <a href="https://bit.ly/joinshecodeafrica" className="shecode_button">Join Us</a>
+        </Section>
 
         <Section 
           title="Attend an Event"
           content="Participate in any of our online or offline events and initiatives inluding bootcamps, Fireside chats, webinars, Mentorship programs and several more!"
-          imgClass="home_side_event justify-self-center"
-          className="-order-1 justify-self-center"
-        />
+          imgClass="home_side_event justify-self-end"
+          className="-order-1"
+        >
+          <a href="https://bit.ly/joinshecodeafrica" className="shecode_button">View Upcoming Events</a>
+        </Section>
 
         <Section 
           title="Be a Partner or Sponsor"
           content="Be a sponsor for our next event via a donation or Partner with our organisation to reach more women in tech across Africa"
-          imgClass="home_side_partner justify-self-center"
+          imgClass="home_side_partner"
           className="order-last"
-        />
+          >
+          <a href="https://bit.ly/joinshecodeafrica" className="shecode_button">Be A Partner</a>
+          <a href="https://bit.ly/joinshecodeafrica" className="shecode_button2 ml-2">Be a Donor</a>
+        </Section>
 
         <Section 
           title="Follow Us on Social Media"
           content="Stay updated with us and be the first to know about all our activities and events, by following our social media pages"
-          imgClass="home_side_follow justify-self-center"
-          className="-order-1 justify-self-end"
-        />
+          imgClass="home_side_follow justify-self-end"
+          className="-order-1"
+          >
+          <a href="https://bit.ly/joinshecodeafrica" className="shecode_button">Learn More</a>
+        </Section>
 
       </div>
     </main>
