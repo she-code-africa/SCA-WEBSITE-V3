0eed0b27d1d1596a31e81d5ae7f016edd625660e
Elizabeth Damilola <leezeelola@gmail.com>
2022-02-01 23:30:05 +0100

    add extra icon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
-// import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'animate.css'
 import './index.css';
 
 ReactDOM.render(
