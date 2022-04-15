f0f14701a19d9d638f6cc16a5cbf48416983a66d
elizabeth <leezeelola@gmail.com>
2022-04-15 01:07:02 +0100

    update forgot pass

--- src/pages/Jobs/ForgotPassword.jsx
+++ src/pages/Jobs/ForgotPassword.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import logo from '../../images/she-code-africa-logo.svg'
 import close from '../../images/cancel.svg'
 import { paths } from '../../utils'
@@ -7,6 +7,15 @@ import { paths } from '../../utils'
 
 
 const ForgotPassword = () => {
+
+  const [email, setEmail] = useState('')
+  const navigate = useNavigate()
+
+  const submitResetPass = () => {
+    navigate(paths.login_org);
+  }
+
+  
   return (
     <main className="py-8 lg:w-4/12 md:w-10/12 w-11/12 mx-auto">
       <Link to={paths.jobs} className="flex justify-end">
@@ -18,9 +27,9 @@ const ForgotPassword = () => {
         <p>Enter your registered email to receive password reset link</p>
       </section>
       <section className="py-5">
-        <form className="mt-5 mb-2 flex flex-col justify-center">
+        <form className="mt-5 mb-2 flex flex-col justify-center" onSubmit={submitResetPass}>
           <div>
-            <input type="email" id="email" class="w-full h-11 bg-white-smoke rounded-[4px] py-[6px] px-3 text-mortar focus:outline-none focus:border-sky-500 focus:ring-sky-500 block sm:text-sm focus:ring-1" placeholder="Email *" required />
+            <input type="email" id="email" class="w-full h-11 bg-white-smoke rounded-[4px] py-[6px] px-3 text-mortar focus:outline-none focus:border-sky-500 focus:ring-sky-500 block sm:text-sm focus:ring-1" placeholder="Email *" required value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
 
           <input type="submit" value="SUBMIT" className="bg-[#F0CDE1] text-primary-main-pink hover:bg-primary-main-pink hover:text-white px-6 py-[15px] my-8 rounded-lg rounded-bl-none transition-colors duration-700" />
