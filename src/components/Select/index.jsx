3b87fda4d44f4a3ad99040a2eea64eefb7c716f5
elizabeth <leezeelola@gmail.com>
2022-04-16 10:08:51 +0100

    update comp

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 
-const Select = ({ value, onChange, children, placeholder }) => {
+const Select = ({ value, onChange, children, placeholder, ...rest }) => {
 
   return (
-    <select className="h-11 w-full my-4 bg-white-smoke rounded-[4px] py-[6px] px-3 text-mortar focus:outline-none focus:border-sky-500 focus:ring-sky-500 block sm:text-sm focus:ring-1 mr-3" value={value} onChange={onChange} placeholder={placeholder}>
+    <select className="h-11 w-full my-4 bg-white-smoke rounded-[4px] py-[6px] px-3 text-mortar focus:outline-none focus:border-sky-500 focus:ring-sky-500 block sm:text-sm focus:ring-1 mr-3" value={value} onChange={onChange} placeholder={placeholder} {...rest}>
       {children}
     </select>
   )
