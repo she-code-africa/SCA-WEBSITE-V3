38242bbfe2fdb4fb5711af2e44af37366131003c
elizabeth <leezeelola@gmail.com>
2022-05-24 22:04:10 +0100

    update services

---
+++ src/services/index.js
@@ -0,0 +1,27 @@
+import api from '../utils/api';
+const baseUrl = process.env.REACT_APP_BASE_URL;
+
+export function getEvents () {
+  return api.get(`${baseUrl}/events`).then((events) => {
+    return events;
+  });
+}
+
+export function getReach () {
+  return api.get(`${baseUrl}/reach`).then((reach) => {
+    return reach;
+  });
+}
+
+export function getPartners () {
+  return api.get(`${baseUrl}/partners`).then((partners) => {
+    return partners;
+  });
+}
+
+export function getTeams () {
+  return api.get(`${baseUrl}/teams`).then((teams) => {
+    return teams;
+  });
+}
+
