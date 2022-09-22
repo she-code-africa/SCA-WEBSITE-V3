2a97634a8efcc57a5c401c37cd93fd63c762281c
elizabeth <leezeelola@gmail.com>
2022-09-22 21:45:18 +0100

    add events

--- src/pages/Events/index.jsx
+++ src/pages/Events/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import { Helmet } from "react-helmet";
 import Header from "../../components/Header";
@@ -6,16 +6,35 @@ import Footer from "../../components/Footer";
 import { Link } from "react-router-dom";
 
 import { getEvents } from "../../services";
-import { apiConstants, upcoming_events, past_events } from "../../utils";
+import { apiConstants } from "../../utils";
+import { sortUpcomingEventByDate, sortPastEventsByDate } from "../../utils/helpers";
 
-import event1 from "../../images/event_image.png";
 
 import Slider from "react-slick";
 import { carouselSettings } from "../../components/Carousel";
 
 const Events = () => {
-  // const events = useQuery(apiConstants.events, getEvents);
-  // console.log({ events });
+  const eventCall = useQuery(apiConstants.events, getEvents);
+  const [upcomingEvents, setUpcomingEvents] = useState([])
+  const [pastEvents, setPastEvents] = useState([])
+  
+  useEffect(() => {
+    if (eventCall.isFetched && eventCall.isSuccess) {
+      const events = eventCall.data
+      const _pastEvents = sortPastEventsByDate(events)
+      const _upcomingEvents = sortUpcomingEventByDate(events)
+      if (_pastEvents.length) {
+        setPastEvents(_pastEvents)
+      }
+      if (_upcomingEvents) {
+        setUpcomingEvents(_upcomingEvents)
+      }
+    }
+    
+    return () => {
+     
+    };
+  }, [eventCall.data, eventCall.isFetched, eventCall.isSuccess])
 
   return (
     <>
@@ -41,31 +60,33 @@ const Events = () => {
       <Header page={"events"} />
       <div className="bg-[#FFF7FC]">
         <main className="container mx-auto bg-[#FFF7FC]">
-          <div className="container mx-auto w-11/12 mb-12 p-3 md:px-8 lg:py-44">
-            <Slider {...carouselSettings}>
-              {upcoming_events.map(({ name, details, id }, index) => {
-                return (
-                  <div key={index} className="py-2">
-                    <p className="font-medium">Upcoming!</p>
-                    <h4 className="text-3xl sm:text-4xl lg:text-5xl w-10/12 sm:7/12 lg:w-6/12 font-bold leading-tight">
-                      {name}
-                    </h4>
-                    <p className="text-lg sm:w-7/12 lg:w-6/12 md:text-xl">
-                      {details}
-                    </p>
-                    <div className="mt-16 mb-5">
-                      <Link
-                        to={"/view/events/" + id}
-                        className="border bg-primary-main-pink px-12 font-semibold rounded border-primary-main-pink py-4 text-white"
-                      >
-                        Learn More
-                      </Link>
+          {upcomingEvents.length ? 
+            <div className="container mx-auto w-11/12 mb-12 p-3 md:px-8 lg:py-44">
+              <Slider {...carouselSettings}>
+                {upcomingEvents.map(({ title, description, _id }, index) => {
+                  return (
+                    <div key={index} className="py-2">
+                      <p className="font-medium">Upcoming!</p>
+                      <h4 className="text-3xl sm:text-4xl lg:text-5xl w-10/12 sm:7/12 lg:w-6/12 font-bold leading-tight">
+                        {title}
+                      </h4>
+                      <p className="text-lg sm:w-7/12 lg:w-6/12 md:text-xl">
+                        {description}
+                      </p>
+                      <div className="mt-16 mb-5">
+                        <Link
+                          to={"/view/events/" + _id}
+                          className="border bg-primary-main-pink px-12 font-semibold rounded border-primary-main-pink py-4 text-white"
+                        >
+                          Learn More
+                        </Link>
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
-            </Slider>
-          </div>
+                  );
+                })}
+              </Slider>
+            </div>
+          : ''}
           <section className="border-[5px] border-white shadow-[0px_4px_250px_rgba(183,5,105,0.15)] bg-white p-8 sm:p-12 lg:p-24 w-11/12 md:w-[87%] mx-auto md:h-[417px]">
             <h4 className="text-primary-main-pink font-bold text-3xl md:text-4xl my-2">
               SCA Events
@@ -88,31 +109,35 @@ const Events = () => {
             </section>
             <section className="container mx-auto md:p-8">
               <div className="grid md:grid-cols-2 gap-x-5 gap-y-10 md:gap-y-20 mx-auto p-6">
-                {upcoming_events.map((event, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="h-auto min-w-[250px] min-h-[250px] sm:min-w-[300px] lg:min-h-[526px] lg:min-w-[526px] place-self-center w-10/12"
-                    >
-                      <div className="border-primary-main-pink border-2">
-                        <img
-                          className="lg:min-h-[526px] lg:w-[550px]"
-                          src={event.image}
-                          alt={`${event.name}`}
-                        />
+                {upcomingEvents.length ?
+                  upcomingEvents.map((event, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="h-auto min-w-[250px] min-h-[250px] sm:min-w-[300px] lg:min-h-[526px] lg:min-w-[526px] place-self-center w-10/12"
+                      >
+                        <div className="border-primary-main-pink border-2">
+                          <img
+                            className="lg:min-h-[526px] lg:w-[550px]"
+                            src={event.image}
+                            alt={`${event.title}`}
+                          />
+                        </div>
+                        <div className="bg-white text-center border-primary-main-pink border-[5px] py-8">
+                          <p className="text-xl font-semibold">{event.title}</p>
+                          <Link
+                            to={"/view/events/" + event._id}
+                            className="bg-primary-main-pink text-white py-2 px-10 rounded-md font-semibold"
+                          >
+                            Learn More
+                          </Link>
+                        </div>
                       </div>
-                      <div className="bg-white text-center border-primary-main-pink border-[5px] py-8">
-                        <p className="text-xl font-semibold">{event.name}</p>
-                        <Link
-                          to={"/view/events/" + event.id}
-                          className="bg-primary-main-pink text-white py-2 px-10 rounded-md font-semibold"
-                        >
-                          Learn More
-                        </Link>
-                      </div>
-                    </div>
-                  );
-                })}
+                    )
+                  })
+                :<div>
+                  We have no upcoming event at the moment
+                </div>}
               </div>
             </section>
           </div>
@@ -128,31 +153,35 @@ const Events = () => {
             </section>
             <section className="container mx-auto md:p-8">
               <div className="grid md:grid-cols-2 gap-x-5 gap-y-10 md:gap-y-20 mx-auto p-6">
-                {past_events.map((event, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="h-auto min-w-[250px] min-h-[250px] sm:min-w-[300px] lg:min-h-[526px] lg:min-w-[526px] place-self-center w-10/12"
-                    >
-                      <div className="border-primary-main-pink border-2">
-                        <img
-                          className="lg:min-h-[526px] lg:w-[550px]"
-                          src={event.image}
-                          alt={`${event.name}`}
-                        />
+                {pastEvents.length ? 
+                  pastEvents.map((event, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="h-auto min-w-[250px] min-h-[250px] sm:min-w-[300px] lg:min-h-[526px] lg:min-w-[526px] place-self-center w-10/12"
+                      >
+                        <div className="border-primary-main-pink border-2">
+                          <img
+                            className="lg:min-h-[526px] lg:w-[550px]"
+                            src={event.image}
+                            alt={`${event.title}`}
+                          />
+                        </div>
+                        <div className="bg-white text-center border-primary-main-pink border-[5px] py-8">
+                          <p className="text-xl font-semibold">{event.title}</p>
+                          <Link
+                            to={"/view/events/" + event._id}
+                            className="bg-primary-main-pink text-white py-2 px-10 rounded-md font-semibold visited:text-primary-dark-pink"
+                          >
+                            Learn More
+                          </Link>
+                        </div>
                       </div>
-                      <div className="bg-white text-center border-primary-main-pink border-[5px] py-8">
-                        <p className="text-xl font-semibold">{event.name}</p>
-                        <Link
-                          to={"/view/events/" + event.id}
-                          className="bg-primary-main-pink text-white py-2 px-10 rounded-md font-semibold visited:text-primary-dark-pink"
-                        >
-                          Learn More
-                        </Link>
-                      </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })
+                : <div>
+                    There's no Past event at the moment
+                  </div>}
               </div>
             </section>
           </div>
