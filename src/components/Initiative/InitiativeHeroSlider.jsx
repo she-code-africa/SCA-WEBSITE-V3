63951e12818678bff6285797c55782a5e78ef11d
daluclemas <chidaluanukam@gmail.com>
2023-06-06 20:53:26 +0100

    removed half images from slider

--- src/components/Initiative/InitiativeHeroSlider.jsx
+++ src/components/Initiative/InitiativeHeroSlider.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
-import imageSliderone from "../../images/initiative/image-slider-1.png";
 import imageSlidertwo from "../../images/initiative/image-slider-2.png";
 import imageSliderthree from "../../images/initiative/image-3.png";
-import imageSliderfour from "../../images/initiative/image-4.png";
 
 const InitiativeHeroSlider = () => {
   const settings = {
@@ -12,19 +10,19 @@ const InitiativeHeroSlider = () => {
     variableWidth: true,
     dots: false,
     infinite: true,
-    slidesToShow: 3,
+    // slidesToShow: 2,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 1000,
-    autoplaySpeed: 3000,
+    speed: 2000,
+    autoplaySpeed: 2000,
     cssEase: "linear",
-    lazyLoad: true,
-    initialSlide: 2,
     responsive: [
       {
         breakpoint: 992,
         settings: {
-          slidesToShow: 2,
+          // slidesToShow: 2,
+          slidesToShow: 1,
         },
       },
 
@@ -39,18 +37,6 @@ const InitiativeHeroSlider = () => {
   return (
     <>
       <Slider {...settings}>
-        <div className=" w-90 mr-[40px] custom-slick-img h-[451px]">
-          <figure className="m-0 p-0 mx-auto h-full" style={{ width: 542 }}>
-            <img
-              src={imageSliderone}
-              alt="slider-one-img"
-              className="w-full h-full"
-              style={{ objectFit: "contain" }}
-              loading="lazy"
-            />
-          </figure>
-        </div>
-
         <div className=" w-90 mr-[40px] custom-slick-img h-[451px]">
           <figure className="m-0 p-0 mx-auto h-full" style={{ width: 334 }}>
             <img
@@ -67,18 +53,6 @@ const InitiativeHeroSlider = () => {
           <figure className="m-0 p-0 mx-auto h-full" style={{ width: 334.03 }}>
             <img
               src={imageSliderthree}
-              alt="slider-one-img"
-              className="w-full h-full"
-              style={{ objectFit: "contain" }}
-              loading="lazy"
-            />
-          </figure>
-        </div>
-
-        <div className=" w-90 mr-[40px] custom-slick-img h-[451px]">
-          <figure className="m-0 p-0 mx-auto h-full" style={{ width: 334 }}>
-            <img
-              src={imageSliderfour}
               alt="slider-one-img"
               className="w-full h-full"
               style={{ objectFit: "contain" }}
