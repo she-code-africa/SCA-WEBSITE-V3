d85a071af8787db1dce1bc8ada5857016d82e41c
daluclemas <chidaluanukam@gmail.com>
2023-09-09 10:15:42 +0100

    update(profile): display jobs created by user on profile page.

--- src/pages/Jobs/Profile/index.jsx
+++ src/pages/Jobs/Profile/index.jsx
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../../../components/Header";
 import Dropdown from "../../../components/Dropdown";
-import { paths } from "../../../utils";
+import { paths, apiConstants } from "../../../utils";
+import { useQuery } from "@tanstack/react-query";
 import Footer from "../../../components/Footer";
+import { getJobs } from "../../../services";
+import Loading from "../../../components/Loading";
+import Error from "../../../components/Error";
 
 const Profile = () => {
   const contactName = JSON.parse(localStorage.getItem("contactName"));
-  const [publishedJobs] = useState([]);
-  const [unPublishedJobs] = useState([]);
+
+  const userData = JSON.parse(localStorage.getItem("userDetails"));
+  const { data, isError, isLoading } = useQuery([apiConstants.jobs], getJobs);
+  const [publishedJobs, setPublishedJobs] = useState([]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      const jobsByUser = data.filter((jobs) => {
+        if (jobs.company !== null) {
+          return jobs.companyProfileId === userData?.userId;
+        } else {
+          return (
+            contactName !== null &&
+            jobs.guestPostMetaData.companyName === contactName.name
+          );
+        }
+      });
+
+      setPublishedJobs(jobsByUser);
+    }
+  }, [isLoading, data, isError]);
 
   return (
     <>
@@ -39,17 +63,42 @@ const Profile = () => {
           <h3 className="text-center job-type-header text-2xl font-medium text-gray-800">
             Published Jobs
           </h3>
-          {publishedJobs.length ? (
-            <ul>
-              {publishedJobs.map((job) => (
-                <li key={job.id}>{job.name}</li>
-              ))}
-            </ul>
+          {isError ? (
+            <Error />
+          ) : isLoading ? (
+            <Loading />
           ) : (
-            <p className="text-center py-8">You have no published jobs</p>
+            <>
+              {publishedJobs.length > 0 ? (
+                <div className="my-6 md:grid grid-cols-2 flex flex-wrap items-stretch gap-10">
+                  {publishedJobs.map((job, index) => (
+                    <article
+                      key={job._id}
+                      className="border border-[#dcdcdc] px-10 py-10 rounded-t-none rounded-br-[50px] rounded-bl-none flex flex-col items-stretch"
+                    >
+                      <h4 className="text-2xl font-medium my-5">{job.title}</h4>
+                      <p className="text-base mb-4">{job.location}</p>
+                      <p className="text-justify overflow-clip text-ellipsis break-words flex-auto mb-8">
+                        {job.description}
+                      </p>
+                      <div className="text-right">
+                        <Link
+                          to={"/view/jobs/" + job.id}
+                          className="uppercase w-auto btn sca-btn pink-btn sca-btn-small"
+                        >
+                          View More
+                        </Link>
+                      </div>
+                    </article>
+                  ))}
+                </div>
+              ) : (
+                <p className="text-center py-8">You have no published jobs</p>
+              )}
+            </>
           )}
         </section>
-        <section className="md:w-7/12 w-11/12 mx-auto __jobs_preview my-10 text-[#707070]">
+        {/* <section className="md:w-7/12 w-11/12 mx-auto __jobs_preview my-10 text-[#707070]">
           <h3 className="text-center job-type-header text-2xl font-medium text-gray-800">
             Unpublished Jobs
           </h3>
@@ -62,7 +111,7 @@ const Profile = () => {
           ) : (
             <p className="text-center py-8">You have no unpublished jobs</p>
           )}
-        </section>
+        </section> */}
       </main>
       <Footer />
     </>
