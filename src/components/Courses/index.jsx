30c6c2e9b07b4336d8aa79eb081749a6a0fa93cd
daluclemas <chidaluanukam@gmail.com>
2023-09-14 14:20:36 +0100

    fix(academy): display the different schools data

--- src/components/Courses/index.jsx
+++ src/components/Courses/index.jsx
@@ -1,19 +1,6 @@
 import React from "react";
-import { apiConstants } from "../../utils";
-import { useQuery } from "@tanstack/react-query";
-import { getAllCourses } from "../../services";
-import Loading from "../Loading";
-import Error from "../Error";
 
-const OnlineCourses = () => {
-  const { data, isLoading, isError } = useQuery(
-    [apiConstants.courses],
-    getAllCourses
-  );
-
-  if (isError) {
-    return <Error />;
-  }
+const OnlineCourses = ({ data }) => {
   return (
     <section className="bg-[#F9F9F9] my-14 lg:my-28 ">
       <div className="max-w-[80rem] 2xl:max-w-[90rem] mx-auto py-12 lg:py-20">
@@ -21,33 +8,26 @@ const OnlineCourses = () => {
           Online Course
         </h2>
         <div className="flex-col md:flex-row flex items-center justify-center gap-5">
-          {isLoading ? (
-            <Loading />
-          ) : (
-            <>
-              {!!data.length &&
-                data.map((content) => {
-                  return (
-                    <div
-                      key={content._id}
-                      className="text-center lg:text-left mt-8 mb-0 lg:mb-24 "
-                    >
-                      <img
-                        className="text-center md:w-96"
-                        src={content.image}
-                        alt="courses"
-                      />
-                      <div className="text-dark-blue text-[13px] font-medium">
-                        <p className="pt-4 pb-1">{content.shortDescription}</p>
-                        <a href={content.applicationLink} className="underline">
-                          Apply
-                        </a>
-                      </div>
-                    </div>
-                  );
-                })}
-            </>
-          )}
+          {data.map((content) => {
+            return (
+              <div
+                key={content._id}
+                className="text-center lg:text-left mt-8 mb-0 lg:mb-24 "
+              >
+                <img
+                  className="text-center md:w-96"
+                  src={content.image}
+                  alt="courses"
+                />
+                <div className="text-dark-blue text-[13px] font-medium">
+                  <p className="pt-4 pb-1">{content.shortDescription}</p>
+                  <a href={content.applicationLink} className="underline">
+                    Apply
+                  </a>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
