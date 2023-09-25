1199b16726bea2318b1e08e5eb2f3f41be338a4a
elizabeth <leezeelola@gmail.com>
2023-09-25 21:34:40 +0100

    fixes: menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -37,6 +37,14 @@ const Header = () => {
   const menus = [
     { to: paths.home, text: "Home" },
     {
+      to: paths.donate,
+      text: "About",
+      list: [
+        { to: paths.about, text: "About SCA" },
+        { to: paths.team, text: "Our Team" },
+      ],
+    },
+    {
       to: paths.about,
       text: "Academy",
       list: schoolList,
@@ -53,7 +61,6 @@ const Header = () => {
         },
         { to: paths.events, text: "Events" },
         { to: paths.initiatives, text: "Initiatives" },
-        { to: paths.jobs, text: "Jobs" },
       ],
     },
     {
@@ -64,14 +71,7 @@ const Header = () => {
         { to: paths.chapters, text: "SCA Chapters" },
       ],
     },
-    {
-      to: paths.donate,
-      text: "About",
-      list: [
-        { to: paths.about, text: "About SCA" },
-        { to: paths.team, text: "Our Team" },
-      ],
-    },
+    { to: paths.jobs, text: "Jobs" },
   ];
   const location = useLocation();
   const [isOpen, setIsOpen] = useState(false);
@@ -128,19 +128,17 @@ const Header = () => {
                   </span>
                   <FontAwesomeIcon
                     icon={faAngleDown}
-                    className={`transition-transform duration-300 ${
-                      selectedMenu === index ? "rotate-180" : null
-                    }`}
+                    className={`transition-transform duration-300 ${selectedMenu === index ? "rotate-180" : null
+                      }`}
                   />
                 </button>
               ) : (
                 <Link
                   to={menu.to}
-                  className={`focus:outline-none focus:ring focus:ring-tutu ${
-                    path === menu.to
-                      ? "text-primary-main-pink border-b border-primary-main-pink font-bold"
-                      : null
-                  }`}
+                  className={`focus:outline-none focus:ring focus:ring-tutu ${path === menu.to
+                    ? "text-primary-main-pink border-b border-primary-main-pink font-bold"
+                    : null
+                    }`}
                 >
                   {menu.text}
                 </Link>
@@ -151,11 +149,10 @@ const Header = () => {
                   {menu.list.map((list, index) => (
                     <li
                       key={index}
-                      className={`font-normal mb-5 ${
-                        path === list.to
-                          ? "text-primary-main-pink border-b border-primary-main-pink font-bold"
-                          : null
-                      }`}
+                      className={`font-normal mb-5 ${path === list.to
+                        ? "text-primary-main-pink border-b border-primary-main-pink font-bold"
+                        : null
+                        }`}
                     >
                       {list?.external ? (
                         <a
@@ -202,33 +199,24 @@ const Header = () => {
           />
         </Link>
 
-        <div
-          className="flex flex-col justify-between items-center cursor-pointer w-[30px] h-[8px] transition-transform duration-300 ease-in-out focus:outline-none focus:ring focus:ring-tutu"
+        <button
+          className="flex flex-col justify-between items-center cursor-pointer w-[30px] h-[10px] transition-transform duration-300 ease-in-out focus:outline-none focus:ring focus:ring-tutu"
           onClick={handleClick}
-          role="button"
-          onKeyUp={(e) => {
-            if (e.key === "Enter") {
-              handleClick();
-            }
-          }}
           tabIndex={0}
         >
           <div
-            className={`line bg-[#1A1A1A] h-[2px] w-full block transition-transform duration-300 ease-in-out translate-y-0 ${
-              isOpen ? "translate-y-[5px] rotate-45" : ""
-            }`}
+            className={`line bg-[#1A1A1A] h-[2px] w-full block transition-transform duration-300 ease-in-out translate-y-0 ${isOpen ? "translate-y-[5px] rotate-45" : ""
+              }`}
           ></div>
           <div
-            className={`line bg-[#1A1A1A] h-[2px] w-full block transition-transform duration-300 ease-in-out translate-y-[6px] ${
-              isOpen ? "scale-x-0" : ""
-            }`}
+            className={`line bg-[#1A1A1A] h-[2px] w-full block transition-transform duration-300 ease-in-out translate-y-[6px] ${isOpen ? "scale-x-0" : ""
+              }`}
           ></div>
           <div
-            className={`line bg-[#1A1A1A] h-[2px] w-full block transition-transform duration-300 ease-in-out translate-y-[12px] ${
-              isOpen ? "translate-y-[0px] -rotate-45" : ""
-            }`}
+            className={`line bg-[#1A1A1A] h-[2px] w-full block transition-transform duration-300 ease-in-out translate-y-[12px] ${isOpen ? "translate-y-[0px] -rotate-45" : ""
+              }`}
           ></div>
-        </div>
+        </button>
 
         {isOpen ? (
           <div className="fixed inset-0 z-[4000] overflow-y-auto animate__animated top-20 bg-white w-full py-5">
@@ -236,11 +224,10 @@ const Header = () => {
               {menus.map((menu, index) => (
                 <li
                   key={index}
-                  className={`font-medium text-xl  ${
-                    path === menu.to
-                      ? "text-primary-main-pink"
-                      : "text-[#1A1A1A] text-opacity-[82%]"
-                  }`}
+                  className={`font-medium text-xl  ${path === menu.to
+                    ? "text-primary-main-pink"
+                    : "text-[#1A1A1A] text-opacity-[82%]"
+                    }`}
                 >
                   {menu?.list ? (
                     <div className="font-medium block">{menu.text}</div>
@@ -255,17 +242,31 @@ const Header = () => {
                   {menu?.list && (
                     <ul className="pt-5 px-5">
                       {menu.list.map((list, index) => (
-                        <li key={index} className="font-normal mb-6">
-                          <Link
-                            to={list.to}
-                            className={` focus:outline-none focus:ring focus:ring-tutu ${
-                              path === list.to
-                                ? "text-primary-main-pink"
-                                : "text-[#1A1A1A] visited:text-[#1A1A1A] visited:text-opacity-[82%] text-opacity-[82%]"
+                        <li
+                          key={index}
+                          className={`font-normal mb-5 ${path === list.to
+                            ? "text-primary-main-pink border-b border-primary-main-pink font-bold"
+                            : null
                             }`}
-                          >
-                            {list.text}
-                          </Link>
+                        >
+                          {list?.external ? (
+                            <a
+                              href={list.to}
+                              target="_blank"
+                              rel="noreferrer"
+                              className="focus:outline-none focus:ring focus:ring-tutu"
+                            >
+                              {list.text}
+                            </a>
+                          ) : (
+                            <Link
+                              to={list.to}
+                              className="focus:outline-none focus:ring focus:ring-tutu"
+                              onClick={() => setIsOpen(false)}
+                            >
+                              {list.text}
+                            </Link>
+                          )}
                         </li>
                       ))}
                     </ul>
@@ -276,7 +277,7 @@ const Header = () => {
           </div>
         ) : null}
       </div>
-    </header>
+    </header >
   );
 };
 
