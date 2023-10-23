ee68ab99806c81b6a3e528c326fdb164809da641
elizabeth <leezeelola@gmail.com>
2023-10-23 18:58:00 +0200

    coc content

--- src/pages/CoC/index.jsx
+++ src/pages/CoC/index.jsx
@@ -72,7 +72,7 @@ const CodeOfConduct = () => {
               If you believe someone is violating the code of conduct, we ask that
               you report it by emailing&nbsp;
               <strong className="font-weight-bolder">
-                <a href="mailto:info@shecodeafrica.org" target="_blank" rel="noreferrer">info@shecodeafrica.org</a>
+                <a href="mailto:community@shecodeafrica.org" target="_blank" rel="noreferrer">community@shecodeafrica.org</a>
               </strong>
               . For more details please see our Reporting Guidelines .
               <br />
