ba0fa969e98c644d0fbe893ffef74aa3bbfbb931
glowwEE <gloryokafor5@gmail.com>
2025-08-12 09:49:17 +0100

    Refactors initiatives page with new hero and card design
    Modernizes the initiatives page by introducing a redesigned hero section and visually updated initiative cards with themed backgrounds and improved typography.
    
    Adds image assets and updates styling to enhance visual identity and user experience.

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,8 @@ module.exports = {
         veryLightGrey: "#CFCFCF",
         "lavendar-blush": "#FFF7FC",
         gains: "#D8D8D8",
+        "Secondary-Grape": "#7D355D",
+        "Primary-Magenta": "#B70569"
       },
       fontSize: {
         15: "15px",
@@ -39,6 +41,10 @@ module.exports = {
       screens: {
         "2md": "992px",
         "2xl": "1950px",
+      },
+      fontFamily: {
+        BebasNeue: ["Bebas Neue", "sans-serif"],
+        figtree: ["Figtree", "sans-serif"],
       },
     },
   },
