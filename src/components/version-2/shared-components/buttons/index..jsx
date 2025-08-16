3c1ab3a97a99b01fc2f3e0103abb0102df971820
daluclemas <chidaluanukam@gmail.com>
2025-08-16 17:30:47 +0100

    update: remove unused props

--- src/components/version-2/shared-components/buttons/index..jsx
+++ src/components/version-2/shared-components/buttons/index..jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CustomButtonComponent = ({
-  btnColor,
   isWithIcon = false,
   icon,
   iconPos,
@@ -15,7 +14,7 @@ const CustomButtonComponent = ({
     <div className={`h-full w-full`}>
       <Link
         to={`${url}`}
-        className={`w-full flex items-center gap-2  justify-center border-white h-[55px] py-[18px] px-8 border-2 ${bgHover} ${customColor}`}
+        className={`w-full flex items-center gap-2 justify-center border-white h-[55px] py-[18px] px-8 border-2 ${bgHover} ${customColor}`}
       >
         {isWithIcon && (
           <>
