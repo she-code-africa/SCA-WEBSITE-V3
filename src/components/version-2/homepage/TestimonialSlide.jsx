28bb4f4ef204459c0a85b5d4f407c67f7d4cb534
daluclemas <chidaluanukam@gmail.com>
2025-10-31 19:54:03 +0100

    update: academy hero section and footer

--- src/components/version-2/homepage/TestimonialSlide.jsx
+++ src/components/version-2/homepage/TestimonialSlide.jsx
@@ -1,47 +1,50 @@
 import React from "react";
+import Loading from "../../Loading";
 
-const TestimonialSlide = ({ testimonialCards }) => {
+const TestimonialSlide = ({ testimonialCards, isLoading }) => {
   const cardStyles = [
     "bg-[#FF8FCE] rotate-3 text-[rgba(92,3,53,1)]",
     "bg-[#8FAB50] xl:mt-24 -rotate-6 text-[rgba(9,74,50,1)]",
     "bg-[#7D355D] rotate-3 text-[rgba(92,3,53,1)]",
   ];
   return (
-    <div className="w-[90%] mx-auto xl:w-full xl:max-w-[1256px]  mt-10 flex flex-col md:flex-row items-center justify-center gap-10 xl:gap-8">
-      {testimonialCards.map((item, i) => (
-        <div
-          className={`w-full max-w-[400px] rounded-2xl overflow-hidden p-0 m-0 ${
-            cardStyles[i % testimonialCards.length]
-          } `}
-          key={i}
-        >
-          <div
-            className={`w-full h-full ${i === 0 && "bg-[#FFB8E0]"} ${
-              i === 1 && "bg-[#DDFF8F]"
-            } ${i === 2 && "bg-[#FFF88F]"} clip-testimonial bg-no-repeat`}
-            style={{
-              backgroundImage: `url(${item.img})`,
-              backgroundSize: "181px 158px",
-            }}
-          >
-            <article className="py-28 px-9 w-full h-full max-w-[350px] ">
-              <p className={`leading-normal text-sm font-medium`}>
-                “Lorem ipsum dolor sit amet consectetur. Et nec sit elementum
-                amet pharetra varius proin eleifend leo. Sagittis aliquet urna
-                suspendisse in ut tincidunt. Magna duis rhoncus ullamcorper
-                massa. Tincidunt eu risus est dapibus. Mi pretium turpis ipsum
-                feugiat et vel eu lacus nec. Adipiscing eget et molestie
-                faucibus arcu metus urna in. Nisi egestas et vitae quam euismod
-                rhoncus enim. Aliquam faucibus mi commodo venenatis diam ante.”
-              </p>
+    <div className="w-[90%] mx-auto  xl:max-w-[1256px]  mt-10 flex flex-col md:flex-row items-center justify-center gap-10 xl:gap-8">
+      {isLoading ? (
+        <>
+          <Loading />
+        </>
+      ) : (
+        <>
+          {testimonialCards?.slice(0, 3).map((item, i) => (
+            <div
+              className={`w-full max-w-[400px] rounded-2xl overflow-hidden p-0 m-0 ${
+                cardStyles[i % testimonialCards.length]
+              } `}
+              key={i}
+            >
+              <div
+                className={`w-full h-full ${i === 0 && "bg-[#FFB8E0]"} ${
+                  i === 1 && "bg-[#DDFF8F]"
+                } ${i === 2 && "bg-[#FFF88F]"} clip-testimonial bg-no-repeat`}
+                style={{
+                  backgroundImage: `url(${item.img})`,
+                  backgroundSize: "181px 158px",
+                }}
+              >
+                <article className="py-28 px-9 w-full h-full max-w-[350px] ">
+                  <p className={`leading-normal text-sm font-medium`}>
+                    {item.testimony}
+                  </p>
 
-              <h4 className="mt-3 font-semibold text-sm leading-normal text-black">
-                - Queen Jane
-              </h4>
-            </article>
-          </div>
-        </div>
-      ))}
+                  <h4 className="mt-3 font-semibold text-sm leading-normal text-black">
+                    - Queen Jane
+                  </h4>
+                </article>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
--- src/components/version-2/homepage/Testimonials.jsx
+++ src/components/version-2/homepage/Testimonials.jsx
@@ -6,58 +6,20 @@ import { getTestimonials } from "../../../services";
 import TestimonialSlide from "./TestimonialSlide";
 
 const OurQueensTestimonials = () => {
-  const { data: testimonials } = useQuery(
-    [apiConstants.testimonials],
-    getTestimonials
-  );
+  const { data: testimonials, isLoading } = useQuery({
+    queryKey: [apiConstants.testimonials],
+    queryFn: () => getTestimonials(),
+  });
+
+  console.log("testimonials", testimonials);
 
   return (
     <section className="w-full relative pt-20 pb-28 lg:pb-40 mt-20 lg:mt-32 testmonialBg">
-      <h3 className="text-5xl lg:text-[64px] font-bold 2md:leading-[82px] text-primary-main-pink hero-text max-w-[474px] w-full mx-auto">
+      <h3 className="text-5xl lg:text-[64px] font-bold 2md:leading-[82px] text-primary-main-pink hero-text max-w-[474px] w-[90%] text-center mx-auto">
         Our Queens’ Experiences
       </h3>
 
-      {/* <div className="w-[90%] mx-auto xl:w-full xl:max-w-[1256px]  mt-10 flex flex-col md:flex-row items-center justify-center gap-10 xl:gap-8">
-        {testimonialCards.map((item, i) => (
-          <div
-            className={`w-full max-w-[400px] rounded-2xl overflow-hidden p-0 m-0 ${
-              i === 0 && "bg-[#FF8FCE] rotate-3"
-            } ${i === 1 && "bg-[#8FAB50] xl:mt-24 -rotate-6"} ${
-              i === 2 && "bg-[#7D355D] rotate-3"
-            } `}
-            key={i}
-          >
-            <div
-              className={`w-full h-full ${i === 0 && "bg-[#FFB8E0]"} ${
-                i === 1 && "bg-[#DDFF8F]"
-              } ${i === 2 && "bg-[#FFF88F]"} clip-testimonial bg-no-repeat`}
-              style={{
-                backgroundImage: `url(${item.img})`,
-                backgroundSize: "181px 158px",
-              }}
-            >
-              <article className="py-28 px-9 w-full h-full max-w-[350px] ">
-                <p className="leading-normal text-sm font-medium">
-                  “Lorem ipsum dolor sit amet consectetur. Et nec sit elementum
-                  amet pharetra varius proin eleifend leo. Sagittis aliquet urna
-                  suspendisse in ut tincidunt. Magna duis rhoncus ullamcorper
-                  massa. Tincidunt eu risus est dapibus. Mi pretium turpis ipsum
-                  feugiat et vel eu lacus nec. Adipiscing eget et molestie
-                  faucibus arcu metus urna in. Nisi egestas et vitae quam
-                  euismod rhoncus enim. Aliquam faucibus mi commodo venenatis
-                  diam ante.”
-                </p>
-
-                <h4 className="mt-3 font-semibold text-sm leading-normal">
-                  - Queen Jane
-                </h4>
-              </article>
-            </div>
-          </div>
-        ))}
-      </div> */}
-
-      <TestimonialSlide testimonialCards={testimonialCards} />
+      <TestimonialSlide testimonialCards={testimonials} isLoading={isLoading} />
     </section>
   );
 };
