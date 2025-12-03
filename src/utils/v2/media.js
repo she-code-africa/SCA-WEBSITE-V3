1e83a0bf15a63cb1dbb69384e2c47ffc8bdaffc9
daluclemas <chidaluanukam@gmail.com>
2025-12-03 12:42:20 +0100

    update: Upload blog content.

--- src/utils/v2/media.js
+++ src/utils/v2/media.js
@@ -4,6 +4,12 @@ import imageThree from "../../images/v2/media/media-conf3.jpg";
 import imageFour from "../../images/v2/media/media-conf4.jpg";
 import imageFive from "../../images/v2/media/media-conf5.jpg";
 
+import blogImg1 from "../../images/v2/media/blog/blogImg1.jpg";
+import blogImg2 from "../../images/v2/media/blog/blogImg2.jpeg";
+import blogImg3 from "../../images/v2/media/blog/blogImg3.jpeg";
+import blogImg4 from "../../images/v2/media/blog/blogImg4.jpg";
+import blogImg5 from "../../images/v2/media/blog/blogImg5.jpg";
+
 export const mediaContent = [
   {
     id: 1,
@@ -94,4 +100,65 @@ export const mediaContent = [
 export const videoContent = [
   "https://www.youtube.com/embed/KAkf1X7oXRY?si=JGdLSkBrZ4fd1EY6",
   "https://www.youtube.com/embed/0KX8lgNX4dc?si=WlI8R6m4MUFoolCT",
+];
+
+export const blogContent = [
+  {
+    title: "She Code Africa Digital Literacy Program Takes Off",
+    type: "She Code Africa Initiatives",
+    description: "First Stop, Oyo State Nigeria.",
+    author: "Winner Bright",
+    dateCreated: "18/11/2025",
+    coverImage: blogImg1,
+    url: "https://www.linkedin.com/pulse/she-code-africa-digital-literacy-program-takes-off-first-azbtf/?trackingId=SI17pHSFSrqWjpg6q2PIKw%3D%3D",
+  },
+  {
+    title: "I want to Dive into Tech but I’m confused",
+    type: "Career Development ",
+    description:
+      "Understanding tech fields and how to decide where you belong.",
+    author: "Lois Choji",
+    dateCreated: "01/01/2023",
+    coverImage: blogImg2,
+    url: "https://medium.com/shecodeafrica/i-want-to-dive-into-tech-but-im-confused-756935b7e185",
+  },
+  {
+    title: "What NOT to do While Applying for a Role in Tech",
+    type: "Career Development",
+    description: "Avoid these mistakes before you hit “Send application”.",
+    author: "Olamide Makinde",
+    dateCreated: "01/03/2023",
+    coverImage: blogImg3,
+    url: "https://medium.com/shecodeafrica/what-not-to-do-while-applying-for-a-role-in-tech-61459e0a25c4",
+  },
+  {
+    title: "She Code Africa Joins 2025 HP Digital Equity Accelerator!",
+    type: "She Code Africa Updates",
+    description:
+      "Scaling digital skills and access for African women through HP partnership.",
+    author: "Winner Bright",
+    dateCreated: "29/04/2025",
+    coverImage: blogImg4,
+    url: "https://medium.com/shecodeafrica/she-code-africa-joins-2025-hp-digital-equity-accelerator-fc61e7a53671",
+  },
+  {
+    title: "Why We Need More Women in Tech",
+    type: "Tech Insights",
+    description: "Closing the gap, one smart woman at a time",
+    author: "Hafsah Emekoma",
+    dateCreated: "13/08/2021",
+    coverImage: blogImg5,
+    url: "https://medium.com/shecodeafrica/why-we-need-more-women-in-tech-a4f677d0ae59",
+  },
+  {
+    title:
+      "She Code Africa Recognized as an Impact Award Winner by Zendesk Foundation’s Tech for Good Program",
+    type: "She Code Africa Updates",
+    description:
+      "She Code Africa wins Tech for Good Impact Award from Zendesk Foundation.",
+    author: "Winner Bright",
+    dateCreated: "12/11/2025",
+    coverImage: blogImg5,
+    url: "https://medium.com/shecodeafrica/she-code-africa-recognized-as-an-impact-award-winner-by-zendesk-foundations-tech-for-good-program-67d2dd33bc7b",
+  },
 ];
