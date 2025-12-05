e3e8d4132f7441fd86b9444fa0899c0373a0d799
daluclemas <chidaluanukam@gmail.com>
2025-12-05 14:15:38 +0100

    update: only show programs that are published

--- src/components/version-2/academy/SchoolProgram.jsx
+++ src/components/version-2/academy/SchoolProgram.jsx
@@ -45,42 +45,48 @@ const SchoolProgram = ({ bgColors, slug, textContent, schoolPrograms }) => {
 
       <div className="w-[90%] mx-auto  lg:max-w-[1182px] -mt-40 relative z-[1]">
         <div className="w-full grid sm:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
-          {schoolPrograms && schoolPrograms.length > 0 ? (
+          {schoolPrograms &&
+          schoolPrograms.filter((program) => program.state === "published")
+            .length > 0 ? (
             <>
-              {schoolPrograms.map(({ title, image, link, briefContent }, i) => (
-                <div
-                  className={`border-2 p-5 bg-white rounded-2xl ${
-                    slug.includes("product")
-                      ? "border-[#094A32] text-[#094A32] card-wwd hover:bg-[#094A32] hover:text-white transition-colors duration-300 cursor-pointer"
-                      : "border-primary-main-pink text-primary-main-pink hover:bg-primary-main-pink hover:text-white transition-colors duration-300 cursor-pointer card-wwd-pink"
-                  }`}
-                  key={i}
-                >
-                  <figure className="h-[334px] w-full rounded-2xl overflow-hidden">
-                    <img
-                      src={image || whatWeDo}
-                      alt="card-pic"
-                      className="object-cover w-full h-full"
-                    />
-                  </figure>
+              {schoolPrograms
+                .filter((program) => program.state === "published")
+                .map(({ title, image, link, briefContent }, i) => (
+                  <div
+                    className={`border-2 p-5 bg-white rounded-2xl ${
+                      slug.includes("product")
+                        ? "border-[#094A32] text-[#094A32] card-wwd hover:bg-[#094A32] hover:text-white transition-colors duration-300 cursor-pointer"
+                        : "border-primary-main-pink text-primary-main-pink hover:bg-primary-main-pink hover:text-white transition-colors duration-300 cursor-pointer card-wwd-pink"
+                    }`}
+                    key={i}
+                  >
+                    <figure className="h-[334px] w-full rounded-2xl overflow-hidden">
+                      <img
+                        src={image || whatWeDo}
+                        alt="card-pic"
+                        className="object-cover w-full h-full"
+                      />
+                    </figure>
 
-                  <article className="mt-5 w-full">
-                    <h4 className="capitalize text-xl font-semibold">
-                      {title}
-                    </h4>
-                    <p className="font-medium text-base mt-4">{briefContent}</p>
-                  </article>
+                    <article className="mt-5 w-full">
+                      <h4 className="capitalize text-xl font-semibold">
+                        {title}
+                      </h4>
+                      <p className="font-medium text-base mt-4">
+                        {briefContent}
+                      </p>
+                    </article>
 
-                  <div className="mt-5 w-full ">
-                    <a
-                      href={link || "#"}
-                      className="text-base w-full flex items-center gap-2"
-                    >
-                      Learn more <FontAwesomeIcon icon={faArrowRight} />{" "}
-                    </a>
+                    <div className="mt-5 w-full ">
+                      <a
+                        href={link || "#"}
+                        className="text-base w-full flex items-center gap-2"
+                      >
+                        Learn more <FontAwesomeIcon icon={faArrowRight} />{" "}
+                      </a>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
             </>
           ) : (
             <>
