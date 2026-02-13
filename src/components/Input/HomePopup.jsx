3cecb76a8940bf747aa666a710ad1669135c9675
daluclemas <chidaluanukam@gmail.com>
2026-02-13 16:45:20 +0100

    feat(home): add popup modal for report on home page

---
+++ src/components/Input/HomePopup.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const HomePopupInput = ({
+  label,
+  name,
+  placeholder,
+  type = "text",
+  onchange,
+  value,
+}) => {
+  return (
+    <div className="w-full mb-4">
+      <label
+        htmlFor={name}
+        className="text-[#211F1F] capitalize text-base font-normal"
+      >
+        {label}
+      </label>
+
+      <div className="w-full h-[52px] mt-2 overflow-hidden border border-primary-main-pink rounded-lg">
+        <input
+          type={type}
+          className="w-full h-full bg-transparent outline-none p-4 placeholder:text-[#ff8fce] text-base"
+          placeholder={placeholder}
+          value={value}
+          onChange={onchange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HomePopupInput;
---
+++ src/components/PopUpModal/home/index.jsx
@@ -0,0 +1,105 @@
+import HomePopupInput from "../../Input/HomePopup";
+import closeBtn from "../../../images/v2/xImg.png";
+import { useState } from "react";
+
+const HomePageModal = ({ closeModal }) => {
+  const [formValues, setFormValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    setFormValues((prev) => ({ ...prev, [name]: e.target.value }));
+  };
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+
+    const { firstName, lastName, email } = formValues;
+
+    if (!firstName || !lastName || !email) {
+      return false;
+    }
+  };
+  return (
+    <div className="fixed top-0 left-0 right-0 bottom-0  z-30 bg-black/50 flex items-center justify-center py-5 overflow-auto">
+      <section className="w-[90%] mx-auto max-w-[1040px] rounded-3xl md:rounded-[50px] flex items-center overflow-hidden">
+        <div className="w-full flex">
+          <div className="w-full bg-[#FFF88F] px-10 py-9 md:py-11 md:px-12 xl:px-20 xl:py-[70px]">
+            <div className="mb-4 lg:hidden flex justify-end">
+              <button
+                className="w-[16px] h-[16px] overflow-hidden"
+                onClick={closeModal}
+              >
+                <img
+                  src={closeBtn}
+                  alt="close button"
+                  className="w-full h-full object-contain"
+                />
+              </button>
+            </div>
+            <h3 className="text-primary-main-pink font-semibold text-[36px] md:text-[64px] hero-text">
+              Download Our 2025 Annual Report here:
+            </h3>
+
+            <form
+              className="w-full  lg:max-w-[345px] mt-[30px] font-figtree"
+              onSubmit={submitForm}
+            >
+              <HomePopupInput
+                label="first name"
+                name="firstName"
+                value={formValues.firstName}
+                onchange={handleChange}
+                placeholder="Type your first name"
+              />
+
+              <HomePopupInput
+                label="last name"
+                name="lastName"
+                value={formValues.lastName}
+                onchange={handleChange}
+                placeholder="Type your last name (surname)"
+              />
+
+              <HomePopupInput
+                label="email"
+                name="email"
+                placeholder="Email address"
+                value={formValues.email}
+                onchange={handleChange}
+                type="email"
+              />
+
+              <div className="w-full max-w-[185px] h-[52px] overflow-hidden rounded-lg">
+                <button className="bg-primary-main-pink text-white text-base w-full h-full hover:bg-[#5C0335] rounded-lg transition-all duration-300">
+                  Download report
+                </button>
+              </div>
+            </form>
+          </div>
+
+          {/*  */}
+          <div className="w-full hidden lg:block max-w-[505px]  popup-bg ">
+            <div className="p-10 w-full flex justify-end">
+              <button
+                className="w-[18px] h-[18px] overflow-hidden"
+                onClick={closeModal}
+              >
+                <img
+                  src={closeBtn}
+                  alt="close button"
+                  className="w-full h-full object-contain"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomePageModal;
